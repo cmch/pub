b2c4e4ab163c1ed057c05c32c409d3ae2e53acff
Colin McHugh <colin.242@gmail.com>
2015-05-24 21:42:05 -0700

    Emacs config checkin

---
+++ .emacs.d/init.el
@@ -0,0 +1,13 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "settings.org"
+                   user-emacs-directory))
+
