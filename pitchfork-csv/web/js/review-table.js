846312e6fc69ccb90f63164e7564d28a5e5dfe9a
Colin McHugh <colin.242@gmail.com>
2015-06-14 07:35:31 -0700

    minimal data table w/ static data

--- pitchfork-csv/web/js/review-table.js
+++ pitchfork-csv/web/js/review-table.js
@@ -1,3 +1,15 @@
 $(document).ready( function () {
-    $('#table_id').DataTable();
+    $('#demo').html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example"></table>' );
+ 
+    $('#example').dataTable( {
+        "data": albums,
+        "columns": [
+            { "title": "Link" },
+            { "title": "Artist" },
+            { "title": "Title" },
+            { "title": "Reviewer"},
+            { "title": "Date"},
+            { "title": "Score"}
+        ]
+    } );      
 } );
